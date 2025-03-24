853bdede3777b958fb197cf1009b36a3a5385c24
Marat F <twinkcy@yandex.ru>
2025-03-24 15:16:14 +0300

    Починил очистку env.

--- glade/interface3.py
+++ glade/interface3.py
@@ -57,7 +57,7 @@ class UnameApp:
         self.lbl_output_3 = builder.get_object("lbl_output_3")
 
         #Выdод об успешной смене env
-        self.lbl_output_env = builder.get_object("lbl_output_env")
+ ##       self.lbl_output_env = builder.get_object("lbl_output_env")
 
         self.window.connect("destroy", Gtk.main_quit)
         self.window.show_all()
@@ -66,14 +66,14 @@ class UnameApp:
 #Меняем пароль VNC
 
     def on_save_button_clicked(self, button):
-        entry_input = self.entry_input.get_text().strip()
+        input_text = self.entry_input.get_text().strip()
         #проверяем на рут тут и дальше в тело вывод полученной инфы!
         if os.geteuid() != 0:
             text_pw_change = "You need to bee root!"
             self.lbl_output_3.set_text(text_pw_change)
         else:    
-            if entry_input:
-                change_vnc = "x11vnc -storepasswd {} /root/.vnc/passwd".format(entry_input)
+            if input_text:
+                change_vnc = "x11vnc -storepasswd {} /root/.vnc/passwd".format(input_text)
                 os.system(change_vnc)
                 text_pw_change = "Пароль от VNC изменен!"
                 self.lbl_output_3.set_text(text_pw_change)
@@ -100,18 +100,20 @@ class UnameApp:
 #Меняем переменные
 
     def on_save_button_clicked_env(self, button):
-        #Проверка пустой строки
-        input_text_env = self.lbl_output_env.get_text().strip()
+        input_text_env = self.entry_input.get_text().strip()
         #проверяем на рут тут и дальше в тело вывод полученной инфы!
         if os.geteuid() != 0:
             text_pw_change = "You need to bee root!"
             self.lbl_output_env.set_text(text_pw_change)
         else:    
-            change_env = "echo {} > /etc/environment".format(input_text_env)
-            os.system(change_env)
-            text_pw_change = "Переменная прокси изменена!"
-            self.lbl_output_3.set_text(text_pw_change)
-
+            if input_text_env:
+                change_env = "echo {} > /etc/environment".format(input_text_env)
+                os.system(change_env)
+                text_pw_change = "Переменная прокси изменена!"
+                self.lbl_output_3.set_text(text_pw_change)
+            else:
+                text_pw_change = "Пустое значение env!"
+                self.lbl_output_3.set_text(text_pw_change)
 
 
 #####################################################################################
@@ -136,7 +138,7 @@ class UnameApp:
     def on_btn_run_3_clicked(self, button):
         print_lacuna = ''
         self.entry_input.set_text(print_lacuna)
-        self.lbl_output.set_text(print_lacuna)
+     #   self.lbl_output.set_text(print_lacuna)
         self.lbl_output_3.set_text(print_lacuna)
         self.lbl_output_env.set_text(print_lacuna)
 
