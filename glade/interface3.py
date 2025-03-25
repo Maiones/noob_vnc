781457a093aa745609d968e34b564c1ca6645cdc
Marat F <faizov@cg.ru>
2025-03-25 09:35:48 +0300

    fix ^.

--- glade/interface3.py
+++ glade/interface3.py
@@ -139,7 +139,6 @@ class UnameApp:
         print_lacuna = ''
         self.entry_input.set_text(print_lacuna)
         self.lbl_output_3.set_text(print_lacuna)
-      #  text_buffer = self.lbl_output_env.get_buffer()
         self.text_buffer_env.set_text(print_lacuna)
 
 #####################################################################################
